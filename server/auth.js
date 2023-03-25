2b0f1dc4b2956b9798102fc78187b49d7840a914
jordan-mann <jr.mannsjn@.com>
2023-03-25 14:33:00 -0500

    installed dotenv

--- server/auth.js
+++ server/auth.js
@@ -1,7 +1,7 @@
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const passport = require('passport');
 const mongoose = require('mongoose');
-const router = requir
+const router = express.Router();
 
 const path = require('path');
 const express = require('express');
@@ -18,27 +18,75 @@ const GOOGLE_CLIENT_ID = "871931103939-s4toc7q1cac7d2vdpvgps6kgp9joc0jm.apps.goo
 const GOOGLE_CLIENT_SECRET = "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";
 
 
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8020/google/callback",
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
-));
+const googleUser = function (passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback',
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        //get the user data from google
+        const newUser = {
+          googleId: profile.id,
+          name: profile.name,
+          dailyEntryId: ''
+        }
+
+        try {
+          //find the user in our database
+          let user = await User.findOne({ googleId: profile.id })
+
+          if (user) {
+            //If user present in our database.
+            done(null, user)
+          } else {
+            // if user is not preset in our database save user data to database.
+            user = await User.create(newUser)
+            done(null, user)
+          }
+        } catch (err) {
+          console.error(err)
+        }
+      }
+    )
+  )
+
+  // used to serialize the user for the session
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+
+  // used to deserialize the user
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user))
+  })
+}
 
 
-  //   function(request, accessToken, refreshToken, profile, done) {
-//     User.findOrCreate(
-//         { 
-//         googleId: profile.id,
-//         name: profile.name,
-//     }, function (err, profile) {
-//       return done(err, profile);
-//     });
-//   }
-//   ));
+    // if user is authenticated the redirected to next page else redirect to login page
+   const ensureAuth = function (req, res, next) {
+      if (req.isAuthenticated()) {
+        return next()
+      } else {
+        res.redirect('/')
+      }
+    }
+    // if user is authenticated and going to login page then redirected to home page if not authenticated redirected to login page  .
+   const ensureGuest = function (req, res, next) {
+      if (!req.isAuthenticated()) {
+        return next();
+      } else {
+        res.redirect('/log');
+      }
+    }
+  
+  
+
+
+
+module.exports.googleUser = googleUser;
+module.exports.ensureAuth = ensureAuth;
+module.exports.ensureGuest = ensureGuest;
+
