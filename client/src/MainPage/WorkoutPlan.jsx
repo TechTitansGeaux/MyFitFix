9a295fb9242441686b034e1191ab9f7b2955d827
neilthemippy <shapironeila@gmail.com>
2023-03-27 12:09:16 -0500

    finished format of the Workoutlist components

--- client/src/MainPage/WorkoutPlan.jsx
+++ client/src/MainPage/WorkoutPlan.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 function WorkoutPlanner () {
-    return <h1>Plan Your Workout</h1>
+
+const workoutState = [];
+
+const workouts, setWorkouts = useState(workoutState);
+
+
+    return (
+  <div>
+    <h1 align='center'>Plan Your Workout</h1>
+    <h2 align='left'>Search for Exercises</h2>
+    <h3 align='right'> Add Exercises To Your Workout</h3>
+    <div className='workout'>
+     <WorkoutList> </WorkoutList>
+     </div>
+</div>
+    )
 }
 
 export default WorkoutPlanner;
