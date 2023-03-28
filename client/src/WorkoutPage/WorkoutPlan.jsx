c201f0671aa35e2e0ee852d0f79ca2726ebc9aa3
dREWbxKewb <drewvasqu1432@gmail.com>
2023-03-28 10:05:04 -0500

    testing

--- client/src/WorkoutPage/WorkoutPlan.jsx
+++ client/src/WorkoutPage/WorkoutPlan.jsx
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
 import data from "./data.json";
 import WorkoutList from './WorkoutList';
-import WorkoutSearch from './WorkoutSearch';
+// import WorkoutSearch from './WorkoutSearch';
 
 
 function WorkoutPlanner() {
 
-const [ exercises, setExercises] = useState(data);
+  const [exercises, setExercises] = useState(data);
 
 
 
-    return (
-  <div>
-    <h1 align='center'>Plan Your Workout</h1>
-    <h2 align='left'>Search for Exercises</h2>
-    <h3 align='right'> Add Exercises To Your Workout</h3>
-    <div className='workoutSearch'>
-      <WorkoutSearch />
+  return (
+    <div>
+      <h1 align='center'>Plan Your Workout</h1>
+      <h2 align='left'>Search for Exercises</h2>
+      <h3 align='right'> Add Exercises To Your Workout</h3>
+      <div className='workoutSearch'>
+        {/* <WorkoutSearch /> */}
+      </div>
+      <div className='workout'>
+        <WorkoutList exercises={exercises} />
+      </div>
     </div>
-    <div className='workout'>
-     <WorkoutList exercises={exercises}/>
-     </div>
-</div>
-    )
+  )
 }
 
 export default WorkoutPlanner;
