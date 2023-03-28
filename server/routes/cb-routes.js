a94b75764f6bb27dc0ba8a04773ba8bc36f49ded
jordan-mann <jr.mannsjn@.com>
2023-03-28 18:38:04 -0500

    work on backend CaloriesBurned feature. Initial feature working

--- server/routes/cb-routes.js
+++ server/routes/cb-routes.js
@@ -1,28 +1,48 @@
 const router = require('express').Router();
+require('dotenv').config();
 const axios = require('axios');
 const { User, Entries, CaloriesIn, CaloriesBurned } = require('../db/index.js');
 
 
+let burn = 0;
+
 router.get('/caloriesBurned', (req, res) => {
-// console.log(req);
+
+const { activity, weight, duration } = req.query;
 
   const options = {
     method: 'GET',
-    url: 'https://calories-burned-by-api-ninjas.p.rapidapi.com/v1/caloriesburned',
-    params: {activity: 'lifting', weight: weight, duration: duration},
+    url: 'https://api.api-ninjas.com/v1/caloriesburned',
+    params: {activity: activity, weight: weight, duration: duration},
     headers: {
-      'X-RapidAPI-Key': '2cbdb847d2mshd5b48913e0fb840p1e26a6jsnb4b7f045b962',
-      'X-RapidAPI-Host': 'calories-burned-by-api-ninjas.p.rapidapi.com'
+      'X-Api-Key': process.env.CALORIES_BURNED_API,
     }
   };
 
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
+  axios.request(options)
+    .then(function (response) {
+      burn = response.data[1].total_calories;
+      res.status(200).send(response.data[1]);
+    })
+    .catch(function (error) {
+    console.error("BIG FAIL");
+    res.sendStatus(500);
   });
 
 })
+
+router.post('/caloriesBurned', (req, res) => {
+  console.log(req.body);
+  const { activity, weight, duration } = req.body;
+  const newCB = new CaloriesBurned({
+    workout: activity,
+    currentWeight: weight,
+    duration: duration,
+    caloriesBurned: burn
+  })
+  newCB.save();
+})
+
 
 module.exports = router;
 
