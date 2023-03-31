6f605b1c82d9be5f241691d92a8e3e2d47d2e2fd
sophiarosely <pheeuh@gmail.com>
2023-03-31 11:57:18 -0500

    Changing content property  to point to the correct paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./client/dist/*.html'],
+  content: ['./client/dist/*.html', 'client/src/**/*.{js,jsx}', 'client/src/*.{js,jsx}', 'client/src/**/**/*.{js,jsx}'],
+  ///Users/sophialy/senior-immersion/MyFitFix/client/src/Calories
   theme: {
     extend: {},
   },
