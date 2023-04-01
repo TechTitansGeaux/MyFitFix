0df7243cf12a94c67f2f286be170f7d7f832dfc1
sophiarosely <pheeuh@gmail.com>
2023-04-01 14:07:00 -0500

    Editing journal card and adding in SVGs

--- client/src/MainPage/Home.jsx
+++ client/src/MainPage/Home.jsx
@@ -36,19 +36,22 @@ function Home() {
     
       // Implementing useEffect to send a GET request to check if the current signed-in user's daily entry was completed 
     axios.get(`/journal-entry/${todaysDate}`)
-      .then(({ data }) => {
-          if ({ data }) {
+      .then((response) => {
+          if (response.data.length !== 0) {
             setJournalMessage('All done. You have submitted your journal entry for today.')
             setIcon(<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
             <path fill="#c8e6c9" d="M44 24c0 11.045-8.955 20-20 20S4 35.045 4 24 12.955 4 24 4s20 8.955 20 20z"/>
             <path fill="#4caf50" d="m34.586 14.586-13.57 13.586-5.602-5.586-2.828 2.828 8.434 8.414 16.395-16.414-2.829-2.828z"/>
             </svg>)
           } else {
-            setJournalMessage('You have not completed a journal entry for today')
+            setJournalMessage('You have not completed a journal entry for today.')
+            setIcon(<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="48" height="48">
+              <path d="M32 3.4A28.59 28.59 0 0 0 3.4 32 28.59 28.59 0 0 0 32 60.6 28.59 28.59 0 0 0 60.6 32 28.59 28.59 0 0 0 32 3.4Zm0 49.2a4.32 4.32 0 1 1 4.31-4.31A4.32 4.32 0 0 1 32 52.6ZM37.23 17 35.6 39a.6.6 0 0 1-.6.56h-6a.6.6 0 0 1-.6-.56l-1.63-22a5.24 5.24 0 1 1 10.46 0Z" data-name="Layer 35" style="fill:#ffc048"/>
+              </svg>)
           }
     })
       .catch((err) => console.log(err, 'Request failed'));
-    })
+    }, [])
 
 
   return (
@@ -56,7 +59,6 @@ function Home() {
 
       {/* START OF NAVIGATION BAR */}
       <div class="flex mb-4">
-
         <div class="bg-white dark:bg-gray-800  xl:hidden flex text-gray-800 hover:text-black focus:outline-none focus:text-black justify-between w-full p-6 items-center">
 
           <div aria-label="toggler" class="flex justify-center items-center">
@@ -145,47 +147,31 @@ function Home() {
         </div>
 
 
-
         {/* Start of Cards*/}
-      
-       
-
-        <div >
+        <div class="grid grid-rows-4 grid-flow-col gap-4gap-x-5 justify-center mx-auto mb-5">
           {/* Start of Welcome Title & Image */}
-          <div>
-          <header class="text-4xl inline-block mt-8 flex justify-center	ml-20">
+          <header class="text-4xl inline-block mt-8 flex justify-center	">
             <img class="rounded-full inline-block gap-x-2 h-12 justify-right" src={image} alt="avatar" />
             Welcome, {name}!
           </header>
-          </div>
-
-{/* <div class="grid grid-rows-4 grid-flow-col gap-4gap-x-5 justify-center mx-auto mb-5"> */}
-<div>
-
-<div className='flex justify-center'>
-<svg className="flex-shrink-0"></svg>
-          {/* CALORIE INTAKE */}
-          <div class="mt-8 mr-2 sm:mx-auto sm:w-full sm:max-w-md  px-10 py-12 bg-gradient-to-br from-sky-600 from-10%  via-sky-400 to-sky-100 to-40% ...">
-            {/* <div class="px-12 py-12"> */}
+          {/* CALORIE TRACKER */}
+          <div class="max-w-sm rounded overflow-hidden shadow-lg row-span-2">
+            <div class="px-6 py-4">
               <div class="font-bold text-xl mb-2">Calorie Intake</div>
               <p class="text-gray-700 text-base">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
               </p>
-            {/* </div> */}
+            </div>
           </div>
           {/* CALORIES BURNED */}
-          <div class="mt-8 ml-2 sm:w-full sm:max-w-md  px-10 py-12 bg-gradient-to-bl from-sky-600 from-10%  via-sky-400 to-sky-100 to-40% ...">
-            {/* <div class="px-12 py-12"> */}
+          <div class="max-w-sm rounded overflow-hidden shadow-lg row-span-2">
+            <div class="px-6 py-4">
               <div class="font-bold text-xl mb-2">Calories Burned</div>
               <p class="text-gray-700 text-base">
-                Today, you have burned: SOME calories
+                {/* Today, you have burned: {calories} calories */}
               </p>
-            {/* </div> */}
+            </div>
           </div>
-        </div>
-
-<div className='flex justify-center'>
-<svg className="flex-shrink-0"></svg>
           {/* JOURNAL */}
           <div className="max-w-sm rounded overflow-hidden shadow-lg rounded-lg ">
             <div className="px-10 py-7 space-x-3">
@@ -196,25 +182,21 @@ function Home() {
                 <span>{icon}
                 {journalMessage}</span>
               </p>
-            {/* </div> */}
+            </div>
           </div>
           {/* WORKOUT PLANNER */}
-          <div class="mt-8 ml-2  sm:w-full sm:max-w-md  px-10 py-12 bg-gradient-to-tl from-sky-600 from-10%  via-sky-400 to-sky-100 to-40% ...">
-            {/* <div class="px-14 py-14"> */}
+          <div class="max-w-sm rounded overflow-hidden shadow-lg row-span-2">
+            <div class="px-6 py-4">
               <div class="font-bold text-xl mb-2">Workout Planner</div>
               <p class="text-gray-700 text-base">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
               </p>
-            {/* </div> */}
-          </div>
-          </div>
-
-
+            </div>
           </div>
         </div>
-
       </div>
-      
+
+
     </div>
 
 
