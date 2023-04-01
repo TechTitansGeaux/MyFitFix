8dd2a752a52ba88a1e4a4cf7d38ab31c2641a0de
jordan-mann <jr.mannsjn@.com>
2023-04-01 11:00:39 -0500

    styling home page and caloriesBurned

--- server/routes/dashboard-routes.js
+++ server/routes/dashboard-routes.js
@@ -1,25 +1,25 @@
 const router = require('express').Router();
 
-const authCheck = (req, res, next) => {
-  if (!req.user) {
-    //if user is not logged in
-    res.redirect('/auth/login');
-  } else {
-    // if logged in
-    next();
-  }
-}
+// const authCheck = (req, res, next) => {
+//   if (!req.user) {
+//     //if user is not logged in
+//     res.redirect('/auth/login');
+//   } else {
+//     // if logged in
+//     next();
+//   }
+// }
 
-router.get('/', authCheck, (req, res) => {
-  res.render('/home', { user: req.user });
-})
+// router.get('/', authCheck, (req, res) => {
+//   res.render('/home', { user: req.user });
+// })
 
 
-// This will RETRIEVE the specific users name, so it can appear on the dashboard
-router.get('/name', (req, res) => {
-  const { name, thumbnail } = req.user
+// // This will RETRIEVE the specific users name, so it can appear on the dashboard
+// router.get('/name', (req, res) => {
+//   const { name, thumbnail } = req.user
 
-  res.send({name: name, thumbnail: thumbnail});
-})
+//   res.send({name: name, thumbnail: thumbnail});
+// })
 
 module.exports = router;
