54a3d96af4bf177e8d8d069225808eb05767570a
janvierjr <bernie@myheartsleeve.com>
2023-07-25 18:22:30 -0500

    create goals-routes.js AND update db/index.js with GoalsSchema

---
+++ server/routes/goals-routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const {
+  User,
+  CaloriesIn,
+  CaloriesBurned,
+  Workout,
+  Journal,
+} = require("../db/index.js");
+
+
+module.exports = router;
