3ef1d0f966a440fe0e959b33469d59d6d5b23f04
janvierjr <bernie@myheartsleeve.com>
2023-07-25 17:21:45 -0500

    update Goals.jsx view

--- client/src/MainPage/Goals.jsx
+++ client/src/MainPage/Goals.jsx
@@ -8,15 +8,20 @@ const Goals = ({ user, ateTotal, dailyBurn, dailyWorkout }) => {
     const navigate = useNavigate();
   return (
     <div className="alert alert-dismissible fade show fixed bottom-0 right-0 left-0 z-[1030] w-full items-center justify-between bg-neutral-900 py-4 px-6 text-center text-white">
-      <div className="flex-hori center-content-hori">
+      <div className="flex-hori center-content">
         <div className="margin-hori-sm">
-          Total Calories Burned: <span>DYNAMIC SUM</span>
+          Total Calories Burned:{" "}
+          <span className="text-amber-500 font-semibold">DYNAMIC SUM</span>
         </div>
         <div className="margin-hori-sm">
-          Total Pounds Lost: <span>DYNAMIC SUM</span>
+          Total Pounds Lost:{" "}
+          <span className="text-amber-500 font-semibold">DYNAMIC SUM</span>
         </div>
-        <button className="margin-hori-sm" onClick={() => navigate("/home")}>
-          See Total Progress
+        <button
+          className="margin-hori-sm py-2 focus:outline-none dark:text-white justify-start hover:text-white focus:bg-sky-500 bg-amber-600	 focus:text-white hover:bg-sky-500 text-white rounded items-center space-x-6 w-48 min-h-max"
+          onClick={() => navigate("/home")}
+        >
+          See Progress Stats
         </button>
       </div>
     </div>
