3da6f7252f6f2778639ad68ff90ea22ee72e9458
wisdomjackie <wisdomjackie@gmail.com>
2023-07-25 13:14:23 -0500

    added messages navigation from tracker component

--- client/src/MessagesPage/Messages.jsx
+++ client/src/MessagesPage/Messages.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 const Messages = () => {
 
+  const navigate = useNavigate();
+
   return (
     <div className="dms">
-      
+
     </div>
   )
 };
