9bd48433238a2b14199de913a24ed861eba61540
janvierjr <bernie@myheartsleeve.com>
2023-07-26 14:09:43 -0500

    add progress stats section on dashboard Home.jsx AND smooth scroll behavior on click

--- client/src/MainPage/Goals.jsx
+++ client/src/MainPage/Goals.jsx
@@ -3,9 +3,15 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 
-const Goals = ({ user, ateTotal, dailyBurn, dailyWorkout }) => {
-    // Navigate to Home page
-    const navigate = useNavigate();
+const Goals = ({
+  user,
+  ateTotal,
+  dailyBurn,
+  dailyWorkout,
+  handleScrollClick,
+}) => {
+  // Navigate to Home page
+  const navigate = useNavigate();
   return (
     <div className="alert alert-dismissible fade show fixed bottom-0 right-0 left-0 z-[1030] w-full items-center justify-between bg-neutral-900 py-4 px-6 text-center text-white">
       <div className="flex-hori center-content">
@@ -19,13 +25,16 @@ const Goals = ({ user, ateTotal, dailyBurn, dailyWorkout }) => {
         </div>
         <button
           className="margin-hori-sm py-2 focus:outline-none dark:text-white justify-start hover:text-white focus:bg-sky-500 bg-amber-600	 focus:text-white hover:bg-sky-500 text-white rounded items-center space-x-6 w-48 min-h-max"
-          onClick={() => navigate("/home")}
+          onClick={() => {
+            // scroll to progress view OR navigate back to home dashboard
+            handleScrollClick() || navigate("/home");
+          }}
         >
           See Progress Stats
         </button>
       </div>
     </div>
   );
-}
+};
 
 export default Goals;
