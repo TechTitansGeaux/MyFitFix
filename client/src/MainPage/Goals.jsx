e5db19f6d3a293f710891beb89f28991a5a12873
janvierjr <bernie@myheartsleeve.com>
2023-07-26 15:16:51 -0500

    update conditional fixed goals v. show stats bar AND updated slider on Goals.jsx

--- client/src/MainPage/Goals.jsx
+++ client/src/MainPage/Goals.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
 
 const Goals = ({
@@ -12,28 +13,101 @@ const Goals = ({
 }) => {
   // Navigate to Home page
   const navigate = useNavigate();
+  const [showGoals, setShowGoals] = useState(false);
+  const [showInput, setShowInput] = useState(true);
+
+  const initialSliderState = {
+    calorieSlider: 0,
+    weightSlider: 0
+  };
+
+  const [sliderNumber, setSliderNumber] = useState(initialSliderState);
+
+  const handleChangeSlider = (e) => {
+    setSliderNumber({ ...sliderNumber, [e.target.name]: e.target.value});
+  }
+
+
+  const submitGoals = () => {
+    console.log('Goals Saved')
+    setShowGoals(!showGoals);
+    setShowInput(!showGoals);
+  }
+
+
   return (
-    <div className="alert alert-dismissible fade show fixed bottom-0 right-0 left-0 z-[1030] w-full items-center justify-between bg-neutral-900 py-4 px-6 text-center text-white">
-      <div className="flex-hori center-content">
-        <div className="margin-hori-sm">
-          Total Calories Burned:{" "}
-          <span className="text-amber-500 font-semibold">DYNAMIC SUM</span>
+    <>
+      {showInput && (
+        <div className="alert alert-dismissible fade show fixed bottom-0 right-0 left-0 z-[1030] w-full items-center justify-between bg-neutral-700 py-4 px-6 text-center text-white">
+          <div className="flex-hori center-content">
+            <div className="margin-hori-sm">
+              Calories Burned Goal:{" "}
+              <span>
+                <input
+                  type="range"
+                  id="calorieSlider"
+                  name="calorieSlider"
+                  min="0"
+                  max="10000"
+                  onChange={handleChangeSlider}
+                />
+              </span>
+              <h3 className="text-amber-500 font-semibold">
+                {sliderNumber.calorieSlider} total calories
+              </h3>
+            </div>
+            <div className="margin-hori-sm">
+              Weight Loss Goal:{" "}
+              <span>
+                <input
+                  type="range"
+                  id="weightSlider"
+                  name="weightSlider"
+                  min="0"
+                  max="500"
+                  onChange={handleChangeSlider}
+                />
+              </span>
+              <h3 className="text-amber-500 font-semibold">
+                {sliderNumber.weightSlider} total lbs
+              </h3>
+            </div>
+            <button
+              className="margin-hori-sm py-2 focus:outline-none dark:text-white justify-start hover:text-white focus:bg-sky-500 bg-amber-500 focus:text-white font-semibold hover:bg-sky-500 text-white rounded items-center space-x-6 w-48 min-h-max"
+              onClick={() => {
+                // scroll to progress view OR navigate back to home dashboard
+                submitGoals();
+              }}
+            >
+              Save My Goals
+            </button>
+          </div>
         </div>
-        <div className="margin-hori-sm">
-          Total Pounds Lost:{" "}
-          <span className="text-amber-500 font-semibold">DYNAMIC SUM</span>
+      )}
+      {showGoals && (
+        <div className="alert alert-dismissible fade show fixed bottom-0 right-0 left-0 z-[1030] w-full items-center justify-between bg-neutral-900 py-4 px-6 text-center text-white">
+          <div className="flex-hori center-content">
+            <div className="margin-hori-sm">
+              Total Calories Burned:{" "}
+              <span className="text-amber-500 font-semibold">DYNAMIC SUM</span>
+            </div>
+            <div className="margin-hori-sm">
+              Total Pounds Lost:{" "}
+              <span className="text-amber-500 font-semibold">DYNAMIC SUM</span>
+            </div>
+            <button
+              className="margin-hori-sm py-2 focus:outline-none dark:text-white justify-start hover:text-white focus:bg-sky-500 bg-amber-500 focus:text-white font-semibold hover:bg-sky-500 text-white rounded items-center space-x-6 w-48 min-h-max"
+              onClick={() => {
+                // scroll to progress view OR navigate back to home dashboard
+                handleScrollClick() || navigate("/home");
+              }}
+            >
+              See Progress Stats
+            </button>
+          </div>
         </div>
-        <button
-          className="margin-hori-sm py-2 focus:outline-none dark:text-white justify-start hover:text-white focus:bg-sky-500 bg-amber-600	 focus:text-white hover:bg-sky-500 text-white rounded items-center space-x-6 w-48 min-h-max"
-          onClick={() => {
-            // scroll to progress view OR navigate back to home dashboard
-            handleScrollClick() || navigate("/home");
-          }}
-        >
-          See Progress Stats
-        </button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
