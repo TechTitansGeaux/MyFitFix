c9f555955b4799b0e18ad0720dbcfee6d6eaf342
janvierjr <bernie@myheartsleeve.com>
2023-07-26 18:03:11 -0500

    create POST route to save user specific goals to DB AND create axio client side POST request

--- server/routes/goals-routes.js
+++ server/routes/goals-routes.js
@@ -8,16 +8,53 @@ const {
   Journal,
 } = require("../db/index.js");
 
-// GET Goals from the DB
+// GET handler to all Goals from the DB // FOR TESTING
 router.get('/', (req, res) => {
-  // res.send({ msg: 'GET test in postman' }); OK in postman
+  Goals.find({})
+    .then((goals) => {
+      res.status(200).send(goals);
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+  })
 })
 
-// POST Goals and save to the DB
+// GET handler to retrieve Goals of a current user from the DB
+router.get('/', (req, res) => {
+})
+
+// POST handler to save or update goals in the DB
 router.post("/", (req, res) => {
-  // res.send({ msg: "POST test in postman" }); OK in postman
+  // get id from user
+  const { _id } = req.user;
+  // get goalCaloriesBurned and goalWeight from request body
+  const { goalCaloriesBurned, goalWeight } = req.body;
+  console.log('req body', req.body)
+  // use mongoose model method to save goals for specific user
+  Goals.findOneAndUpdate({ user: _id }, {
+    goalCaloriesBurned: goalCaloriesBurned,
+    goalWeight: goalWeight
+  })
+    .then((user) => {
+      if (user) {
+        console.log("SUCCESS in UPDATE Goals for this user");
+        res.sendStatus(200);
+      } else {
+        Goals.create({
+          user: _id,
+          goalCaloriesBurned: goalCaloriesBurned,
+          goalWeight: goalWeight,
+        });
+        res.sendStatus(201);
+      }
+    })
+    .catch((err) => {
+      console.error("FAILED to send Goals Post request:", err);
+      res.sendStatus(500);
+    });
 });
 
 
-// STRETCH // UPDATE Goals in DB
+// UPDATE Goals in DB for totalCaloriesBurned and updatedWeight
+
 module.exports = router;
