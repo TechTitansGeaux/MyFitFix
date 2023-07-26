aec0cdc15f8dad90689b99400ec8acf1a7626e73
Student <student@Bens-OpSpark-Macbook.local>
2023-07-26 13:11:05 -0500

    manuallyMerged

--- server/app.js
+++ server/app.js
@@ -3,8 +3,8 @@ const authRoutes = require('./routes/auth-routes');
 const journalRoutes = require('./routes/journal-routes');
 const dashboardRoutes = require('./routes/dashboard-routes');
 const cbRoutes = require('./routes/cb-routes');
-const nutritionRoutes = require('./routes/nutrition-routes')
-const workoutRoutes = require('./routes/workout-routes')
+const nutritionRoutes = require('./routes/nutrition-routes');
+const workoutRoutes = require('./routes/workout-routes');
 const passportSetup = require('./config/passport-setup');
 const dotenv = require('dotenv');
 const cookieSession = require('cookie-session');
@@ -13,18 +13,24 @@ const passport = require('passport');
 const path = require('path');
 const feedRoutes = require('./routes/feed-routes');
 const userRoutes = require('./routes/user-routes');
-
+require('dotenv').config();
+// socket io variables
+const http = require('http');
+const { Server } = require('socket.io');
+// import cors
+const cors = require('cors');
 
 const CLIENT_PATH = path.join(__dirname, '..', 'client', 'dist');
 const app = express();
 
-
 //Set up view engine
 app.use(express.static(CLIENT_PATH));
-app.use(cookieSession({
-  maxAge: 24 * 60 * 60 * 1000,
-  keys: [process.env.cookieKey]
-}));
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [process.env.cookieKey],
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -41,6 +47,39 @@ app.use('/cb', cbRoutes);
 app.use('/workout', workoutRoutes);
 app.use('/feed', feedRoutes);
 app.use('/users', userRoutes);
+
+// socket io server
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:8020',
+  },
+});
+
+// events for socket io server to listen for
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('send_message', (data) => {
+    socket.broadcast.emit('receive_message', data.message);
+  });
+});
+
+// register middleware to check user connection by username
+// io.use((socket, next) => {
+//   const username = socket.handshake.auth.name;
+//   if (!username) {
+//     return next(new Error("invalid username"));
+//   }
+//   socket.username = username;
+//   next();
+// });
+
+// set socket io server to listen on a separate port
+server.listen(3000, () => {
+  console.log('Socket.io server is running!');
+});
+
 //Create home route
 // app.get('/', (req, res) => {
 //   res.render('home', { user: req.user });
@@ -54,4 +93,4 @@ app.get('*', (req, res) => {
 //Listens to the app server convos (aka listening to request)
 app.listen(8020, () => {
   console.log('app now listening for request at:', 'http://localhost:8020');
-})
+});
--- server/db/index.js
+++ server/db/index.js
@@ -3,42 +3,51 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const mongoUri = 'mongodb://127.0.0.1:27017/fitfix';
-mongoose.connect(mongoUri)
-  .then(() => console.info(('Connected to database: "fitfix"')))
-  .catch((err) => console.error(('Could not connect to database'), err));
+mongoose
+  .connect(mongoUri)
+  .then(() => console.info('Connected to database: "fitfix"'))
+  .catch((err) => console.error('Could not connect to database', err));
 
-  const UserSchema = new Schema({
-    name: String,
-    googleId: { type: String, unique: true },
-    thumbnail: String,
-    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    journalEntries: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Journal' }],
-  });
-  
+const UserSchema = new Schema({
+  name: String,
+  googleId: { type: String, unique: true },
+  thumbnail: String,
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  journalEntries: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Journal' }],
+});
 
+const GoalsSchema = new Schema(
+  {
+    goalCaloriesBurned: Number,
+    goalWeight: Number,
+    totalCaloriesBurned: Number,
+    updatedWeight: Number,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  },
+  { timestamps: true }
+);
 
 const CaloriesInSchema = new Schema({
   foodList: { type: Array, default: [] },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  date: String
-})
+  date: String,
+});
 
 const CaloriesBurnedSchema = new Schema({
   workout: String,
-  currentWeight: {type: Number, required: true },
-  duration: {type: Number, required: true },
+  currentWeight: { type: Number, required: true },
+  duration: { type: Number, required: true },
   caloriesBurned: Number,
   date: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
-
 const WorkoutEntrySchema = new Schema({
-exercise: {type: Array, unique: true },
-date: String,
-user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-})
+  exercise: { type: Array, unique: true },
+  date: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+});
 
 const JournalEntrySchema = new Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
@@ -58,14 +67,12 @@ const NotificationSchema = new Schema({
   read: { type: Boolean, default: false },
 });
 
-
-
-
 module.exports = {
   User: model('User', UserSchema),
+  GoalsSchema: model('Goals', GoalsSchema),
   CaloriesIn: model('CaloriesIn', CaloriesInSchema),
   CaloriesBurned: model('CaloriesBurned', CaloriesBurnedSchema),
   Workout: model('Workout', WorkoutEntrySchema),
   Journal: model('Journal', JournalEntrySchema),
-  Notification: model('Notification', NotificationSchema)
+  Notification: model('Notification', NotificationSchema),
 };
