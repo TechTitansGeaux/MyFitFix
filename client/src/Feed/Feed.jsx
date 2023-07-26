08e03613f12a39b400014daa1f48c26bc20655ba
Student <student@Bens-OpSpark-Macbook.local>
2023-07-25 20:00:38 -0500

    addedComponent

---
+++ client/src/Feed/Feed.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Feed = () => {
+  return (
+    <div className='feed'>
+      <header className='feed_header'>
+        <h2>Feed</h2>
+      </header>
+    </div>
+  )
+}
+
+export default Feed;
